5299edffe140d00b7366d40b092d2561d40a1602
nabid050 <nabid.hasan909@gmail.com>
2023-02-18 18:15:22 +0600

    newly-added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import { RouterProvider } from 'react-router-dom';
 import './App.css';
+import router from './Components/Routes/Router/Router';
 
 function App() {
   return (
     <div className="App">
-      
+      <RouterProvider router = {router}></RouterProvider>
     </div>
   );
 }
